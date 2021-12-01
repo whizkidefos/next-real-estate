a2dd9498ec803162cb34de966081e7f0709e77bc
Efosa Igbinehi <whizkidefos@users.noreply.github.com>
2021-12-01 11:49:44 +0000

    Added Banner sub-component and image config domain

--- pages/index.js
+++ pages/index.js
@@ -2,10 +2,45 @@ import Link from 'next/link';
 import Image from 'next/image';
 import { Flex, Box, Text, Button } from '@chakra-ui/react';
 
+const Banner = ({ purpose, imageUrl, title1, title2, description1, description2, linkName, buttonText }) => (
+  <Flex flexWrap='wrap' justifyContent='center' alignItems='center' m='10'>
+    <Image src={imageUrl} width={500} height={300} alt='banner' />
+    <Box p='5'>
+      <Text color='gray.500' fontSize='sm' fontWeight='medium'>{purpose}</Text>
+      <Text fontSize='3xl' fontWeight='bold'>{title1} <br />{title2}</Text>
+      <Text fontSize='lg' paddingTop='3' paddingBottom='3' color='gray.700'>{description1} <br /> {description2}</Text>
+      {/* <Text fontSize='xl' bgColor='blue.300' color='white' fontWeight='medium'>{purpose}</Text> */}
+      <Button fontSize='xl'>
+        <Link href={linkName}>{buttonText}</Link>
+      </Button>
+    </Box>
+  </Flex>
+)
+
 export default function Home() {
   return (
     <div className="home">
       <h1>Hello world</h1>
+      <Banner
+        purpose='RENT A HOME'
+        title1='Rental Homes for'
+        title2='Everyone'
+        description1=' Explore from Apartments, builder floors, villas'
+        description2='and more'
+        buttonText='Explore Renting'
+        linkName='/search?purpose=for-rent'
+        imageUrl='https://bayut-production.s3.eu-central-1.amazonaws.com/image/145426814/33973352624c48628e41f2ec460faba4'
+      />
+      <Banner
+        purpose='BUY A HOME'
+        title1=' Find, Buy & Own Your'
+        title2='Dream Home'
+        description1=' Explore from Apartments, land, builder floors,'
+        description2=' villas and more'
+        buttonText='Explore Buying'
+        linkName='/search?purpose=for-sale'
+        imageUrl='https://bayut-production.s3.eu-central-1.amazonaws.com/image/110993385/6a070e8e1bae4f7d8c1429bc303d2008'
+      />
     </div >
   )
 }
